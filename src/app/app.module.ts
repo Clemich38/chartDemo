5f438caa82ad402fac8453b49c912799a783d594
Clemich38 <clement.letreguilly@gmail.com>
2017-01-16 12:00:50 +0900

    First line chart!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 
-import '../../node_modules/chart.js/dist/Chart.bundle.min.js'; // and
-import { ChartsModule } from 'ng2-charts/components/charts/charts';
+// import '../../node_modules/chart.js/dist/Chart.bundle.min.js'; // and
+// import { ChartsModule } from 'ng2-charts/components/charts/charts';
 
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 
+import {ChartComp} from '../components/ChartComp';
+
 @NgModule({
   declarations: [
     MyApp,
     Page1,
-    Page2
+    Page2,
+    ChartComp
   ],
   imports: [
-    ChartsModule,
+    // ChartsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/page2/page2.ts
+++ src/pages/page2/page2.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
+import {ChartComponent} from '../../components/chartComponent';
 
 @Component({
   selector: 'page-page2',
@@ -8,9 +9,19 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class Page2 {
 
+  public temp: number[];
+  public hours: string[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
-    
+    this.temp = [];
+    this.hours = [];
+
+    for (let i = 0; i < 10; i++) {
+      this.temp.push(i);
+      this.hours.push(i + 'h');
+    }
+
   }
 
 }
